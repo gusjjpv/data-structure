b47c24adaaab6eca31c50df864ea3afbf0fd1d93
Joao Gustavo <joaogustavolima084@gmail.com>
2023-12-05 19:40:23 -0300

    Update 01_da_lista_de_ponteiro.c

--- ponteiros/exercicios/01_da_lista_de_ponteiro.c
+++ ponteiros/exercicios/01_da_lista_de_ponteiro.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-    int x, y, *p; //Declaração de três variáveis inteiras: x, y e um ponteiro p.
+    int x, y, *p; //Declaração de três variáveis inteiras: x, y e um ponteiro int p.
     y = 0; //Atribuição de 0 à variável y.
     p = &y; //O ponteiro p recebe o endereço de y.
     x = *p; //x recebe o valor apontado por p, que é o valor de y (0).
@@ -10,6 +10,6 @@ int main(void)
     --x; //Decrementa x em 1. Agora x é 3.
     (*p) += x; //Adiciona o valor de x ao valor apontado por p (que é o valor de y). Agora y é 4.
     printf("valor de x: %d.\t valor de y: %d.\t valor de p: %p",x,y,*p);
-
+    
     return 0;
 }
