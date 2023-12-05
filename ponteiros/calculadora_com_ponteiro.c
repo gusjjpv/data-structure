bf690709dc35296c0e0bec3c98548f7399af4d34
Joao Gustavo <joaogustavolima084@gmail.com>
2023-12-05 19:41:00 -0300

    função de pedir numero

--- ponteiros/calculadora_com_ponteiro.c
+++ ponteiros/calculadora_com_ponteiro.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
 // 04/12/2023
 
@@ -27,46 +29,57 @@ int calcula(int x, int y, int (*operacao)(int, int))
     return (*operacao)(x, y);
 }
 
+int primeiroNum()
+{
+    int a;
+    printf("Informe o primeiro numero: \n");
+    scanf("%d", &a);
+    return a;
+}
+
+int segundoNum()
+{
+    int a;
+    printf("Informe o segundo numero: \n");
+    scanf("%d", &a);
+    return a;
+}
+
 int main(void)
 {
-    int escolha, num1, num2, resultado;
+    setlocale(LC_ALL, "portuguese");
+
+    int escolha = 0, num1 = 0, num2 = 0, resultado;
     do
     {
-        printf("Escolha a opcao:\n[1]-soma\n[2]-subtracao\n[3]-multiplicacao\n[4]-divisao\n[5]-sair\n");
+
+        printf("Escolha a opcaoo:\n[1]-soma\n[2]-subtracao\n[3]-multiplicacao\n[4]-divisao\n[5]-sair\n");
         scanf("%d", &escolha);
         system("cls");
         switch (escolha)
         {
         case 1:
-            printf("Informe o primeiro numero: \n");
-            scanf("%d", &num1);
-            printf("Informe o segundo numero: \n");
-            scanf("%d", &num2);
+            num1 = primeiroNum();
+            num2 = segundoNum();
             resultado = calcula(num1, num2, soma);
             printf("Resultado da soma: %d\n", resultado);
             break;
         case 2:
-            printf("Informe o primeiro numero: \n");
-            scanf("%d", &num1);
-            printf("Informe o segundo numero: \n");
-            scanf("%d", &num2);
+            num1 = primeiroNum();
+            num2 = segundoNum();
             resultado = calcula(num1, num2, subtracao);
             printf("Resultado da subtracao: %d\n", resultado);
             break;
         case 3:
-            printf("Informe o primeiro numero: \n");
-            scanf("%d", &num1);
-            printf("Informe o segundo numero: \n");
-            scanf("%d", &num2);
+            num1 = primeiroNum();
+            num2 = segundoNum();
             resultado = calcula(num1, num2, multiplicacao);
             printf("Resultado da multiplicacao: %d\n", resultado);
             break;
         case 4:
 
-            printf("Informe o primeiro numero: \n");
-            scanf("%d", &num1);
-            printf("Informe o segundo numero: \n");
-            scanf("%d", &num2);
+            num1 = primeiroNum();
+            num2 = segundoNum();
             if (num2 == 0)
             {
                 printf("ERRO!!! operacao por zero\n");
@@ -78,9 +91,11 @@ int main(void)
         case 5:
             break;
         default:
-            printf("/nOperação invalida/nEscolha novamente:\n");
+            printf("\n Operacaoo invalida\nEscolha novamente:\n");
             break;
         }
+
     } while (escolha != 5);
+
     return 0;
 }
