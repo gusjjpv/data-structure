238a2387071af3dd88098e84501a3d4fb126e36d
Joao Gustavo <joaogustavolima084@gmail.com>
2023-12-05 19:40:11 -0300

    Update 03_da_lista_de_ponteiro.c

--- ponteiros/exercicios/03_da_lista_de_ponteiro.c
+++ ponteiros/exercicios/03_da_lista_de_ponteiro.c
@@ -2,19 +2,17 @@
 
 int main(void){
 
-    int a, b, c, d;
-    int *p1;
-    int *p2 = &a;
-    int *p3 = &c;
-    p1 = p2; // p1 vai receber o endereço que p2 esta apontando
-    *p2 = 10;
-    b = 20;
-    int **pp;
-    pp = &p1;
-    *p3 = **pp;
-    int *p4 = &d;
-    *p4 = b+(*p1)++;
-    printf("a:%d\tb:%d\tc:%d\td:%d\t",a,b,c,d);
-    
+    int a,b,c,d; //declaração das variaveis;
+    int *p1; //declaração de um ponteiro para inteiros;
+    int *p2 = &a; // declara outro ponteiro de inteiro e atribui o endereço de a;
+    int *p3 = &c; // declara outro ponteiro de inteiro e atribui o endereço de c;
+    p1 = p2; // p1 vai receber o valor que p2 armazena, que é o endereço de a, ent podemos modificar a apatir de p1; 
+    *p2 = 10; // vai ser atribuido ao conteudo de p2 o valor 10;
+    b = 20; // é atribuido o valor 20 a b;
+    int **pp;// é declarado um ponteiro para um ponteiro de inteiro;
+    pp = &p1; // pp vai receber o endereço de p1;
+    *p3 = **pp;// o conteudo de p3(valor armazenado em c), vai receber o conteudo armazenado no conteudo do conteudo de p1(valor de a, a agora recebe o valor de c)
+    int *p4 = &d; //é declarado p4 e atribuido o endereço de d
+
     return 0;
 }
