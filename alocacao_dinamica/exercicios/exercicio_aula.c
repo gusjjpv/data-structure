405c2ebeadeaa4cc3699eaec6bbe4064f1c407dd
Joao Gustavo <joaogustavolima084@gmail.com>
2023-12-08 11:15:11 -0300

    update

---
+++ alocacao_dinamica/exercicios/exercicio_aula.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    aloque dinamicamente uma matriz de inteiros de 5x5 e preencha cada elemento da matriz com o seu respectivo indice (matriz[i][j] = i*5 + j). em seguida imprima a matriz
+    nota: liberar a memoria dps de alocar o uso da matriz.
+*/
+
+int main(void){
+    
+    int linhas = 5, colunas = 5, i,j; int **matriz = (int **) malloc(linhas * sizeof(int *));for(i = 0; i < linhas; i++){matriz[i] =(int**)malloc(colunas * sizeof(int *));}
+
+    for()
+
+    return 0;
+}
---
+++ alocacao_dinamica/multiplicacao_matriz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    int linhasA, colunasA, linhasB, colunasB, i,j,k;
+    printf("Entre com o numero de linhas e colunas da matriz A: ");
+    scanf
+
+    return 0;
+}
