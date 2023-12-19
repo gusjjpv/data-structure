049cebe16101e2e07e19a0b9b9b4adec3471c0db
Joao Gustavo <joaogustavolima084@gmail.com>
2023-12-19 11:20:51 -0300

    Update fruteira.c

--- struct/exercicios/fruteira.c
+++ struct/exercicios/fruteira.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <locale.h>
 
-typedef struct frutas{
+typedef struct frutas
+{
 
     char nome[20];
     char cor[20];
-    char sabor[50];
+    char sabor[200];
     int quantidade;
 
-}Frutas;
+} Frutas;
 
-void cadastroFrutas(Frutas *frutas, int tamanho){
+int qtdFrutasCad(int quantidade)
+{
+    printf("informe a quantidade de frutas que deseja cadastrar: ");
+    scanf("%d", &quantidade);
+
+    system("cls");
+
+    return quantidade;
+}
+
+void cadastroFrutas(Frutas *frutas, int tamanho)
+{
     int i;
-    for(i=0;i<tamanho;i++){
+    for (i = 0; i < tamanho; i++)
+    {
         printf("Informe o nome da fruta: ");
         scanf(" %[^\n]", frutas[i].nome);
         printf("Informe a cor da fruta: ");
@@ -21,36 +36,74 @@ void cadastroFrutas(Frutas *frutas, int tamanho){
         scanf(" %[^\n]", frutas[i].sabor);
         printf("Informe a quantidade da fruta: ");
         scanf("%d", &frutas[i].quantidade);
+        system("cls");
     }
 }
 
-void imprimir(Frutas *frutas, int tamanho){
+void imprimir(Frutas *frutas, int tamanho)
+{
     int i;
-    for(i=0;i<tamanho;i++){
+    for (i = 0; i < tamanho; i++)
+    {
         printf("\nNome: %s\nCor: %s\nSabor: %s\nQuantidade: %d", frutas[i].nome, frutas[i].cor, frutas[i].sabor, frutas[i].quantidade);
     }
 }
 
-void mostrasFrutasCad(Frutas *frutas, int tamanho){
+void mostrasFrutasCad(Frutas *frutas, int tamanho)
+{   
     int i;
     printf("Frutas cadastradas: \n");
-    for(i=0;i<tamanho;i++){
-        printf("%s\n", frutas[i].nome);
+    for (i = 0; i < tamanho; i++)
+    {
+        printf("Nome: %s\n", frutas[i].nome);
     }
 }
 
-int main(void){
+void buscaFruta(Frutas *frutas, int tamanho, char nome[])
+{
+    int i;
+    for (i = 0; i < tamanho; i++)
+    {
+        if (strcmp(frutas[i].nome, nome) == 0)
+        {
+            printf("Nome: %s\nCor: %s\nSabor: %s\nQuantidade: %d", frutas[i].nome, frutas[i].cor, frutas[i].sabor, frutas[i].quantidade);
+        }
+    }
+}
 
-    int i, qtdFrutas;
+int main(void)
+{
 
-    printf("Informe a quantidade de frutas: ");
-    scanf("%d", &qtdFrutas);
+    int qtdFrutas, busca;
+    char nome[20];
 
-    //alocando memoria para o vetor de struct
-    Frutas *frutas = (Frutas*) malloc(qtdFrutas* sizeof(Frutas));
-    if(frutas == NULL){
+    qtdFrutas = qtdFrutasCad(qtdFrutas);
+
+    setlocale(LC_ALL, "Portuguese");
+
+
+    // alocando memoria para o vetor de struct
+    Frutas *frutas = (Frutas *)malloc(qtdFrutas * sizeof(Frutas));
+    if (frutas == NULL)
+    {
         printf("Memoria insuficiente");
         exit(1);
+    }
+
+    cadastroFrutas(frutas, qtdFrutas);
+    imprimir(frutas, qtdFrutas);
+
+    printf("\nDeseja buscar uma fruta?\n[1]-Sim\n[2]-Não");
+    scanf("%d", &busca);
+    system("cls");
+
+    if (busca == 1)
+    {
+        mostrasFrutasCad(frutas, qtdFrutas);
+        printf("Informe o nome da fruta: ");
+        scanf(" %[^\n]", nome);
+        system("cls");
+        buscaFruta(frutas, qtdFrutas, nome);
     }
 
     return 0;
