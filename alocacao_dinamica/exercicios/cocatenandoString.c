597f3781f30ba908932db4bd88e7cf19b9a44f00
gusDev <joaogustavolima084@gmail.com>
2024-01-22 17:27:59 -0300

    Create cocatenandoString.c

---
+++ alocacao_dinamica/exercicios/cocatenandoString.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//2. Escreva uma função que receba duas strings como entrada e retorne uma nova string que seja a concatenação das duas. 
+
+void concatenarString(char )
+
+int main(void){
+
+
+
+    return 0;
+}
