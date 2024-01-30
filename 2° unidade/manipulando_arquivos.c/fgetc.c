32fd1ea58d33f0aadc19b0221db5a907e62413f1
gusDev <joaogustavolima084@gmail.com>
2024-01-29 22:38:12 -0300

    Create fgetc.c

---
+++ 2° unidade/manipulando_arquivos.c/fgetc.c
@@ -0,0 +1,38 @@
+/*
+    funçoes para ler arquivos
+   int fscanf( FILE* fp, char* formato,...);
+   int fgetc(FILE* fp);
+   char* fgets(char* s,int n, FILE* fp);
+  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    // cria o arquivo: entrada_escrita.txt no modo "w" write
+    FILE *arquivo = fopen("entrada.txt", "w");
+    char s[20];
+
+    if (arquivo == NULL)
+    {
+        printf("Erro na abertura do arquivo!\n");
+        exit(1);
+    }
+    else
+    {
+        printf("Arquivo foi gerado com sucesso!!\n");
+    }
+    // usando o fgetc para ler uma string do arquivo
+    //fgetc(FILE* fp);
+    fgetc(arquivo);
+    // imprime a string lida
+    printf("%c\n", s);
+
+    if (!fclose(arquivo))
+    {
+        printf("Arquivo fechado com sucesso!\n");
+    }
+
+    return 0;
+}
