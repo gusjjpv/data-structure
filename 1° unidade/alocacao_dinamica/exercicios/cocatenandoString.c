5731cdb4c2f294e9468dd8a3af79d918473b72e2
gusDev <joaogustavolima084@gmail.com>
2024-02-01 08:53:47 -0300

    tinha esquecido de commitar

--- 1° unidade/alocacao_dinamica/exercicios/cocatenandoString.c
+++ 1° unidade/alocacao_dinamica/exercicios/cocatenandoString.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//2. Escreva uma função que receba duas strings como entrada e retorne uma nova string que seja a concatenação das duas. 
+char* concatenaStrings(const char* str1, const char* str2) {
+    int i, j;
+    for (i = 0; str1[i] != '\0'; ++i);
+    for (j = 0; str2[j] != '\0'; ++j);
 
-void concatenarString(char )
+    char* str3 = malloc(i + j + 1);
 
-int main(void){
+    for (i = 0; str1[i] != '\0'; ++i)
+        str3[i] = str1[i];
 
+    for (j = 0; str2[j] != '\0'; ++j)
+        str3[i+j] = str2[j];
 
+    str3[i+j] = '\0';
+
+    return str3;
+}
+
+int main() {
+    const char* str1 = "Olá, ";
+    const char* str2 = "mundo!";
+    char* resultado = concatenaStrings(str1, str2);
+
+    printf("%s\n", resultado);  // Deve imprimir: "Olá, mundo!"
+
+    free(resultado);  // Não se esqueça de liberar a memória alocada!
 
     return 0;
 }
