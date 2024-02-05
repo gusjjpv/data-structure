e8cc79f089dc50d13f331e2f279853ed8f6f5260
gusDev <joaogustavolima084@gmail.com>
2024-02-05 15:28:48 -0300

    Create aluno.c

---
+++ 2° unidade/TAD/teoria/aluno.c
@@ -0,0 +1,40 @@
+/*TAD: aluno*/
+#include "aluno.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+struct aluno
+{
+    char nome[100];
+    int matricula;
+    float IRA;
+};
+
+Aluno *recebe_dados(void)
+{
+    Aluno *estudante = (Aluno *)malloc(sizeof(Aluno));
+    if (estudante == NULL)
+    {
+        printf("Erro ao alocar memória!\n");
+        exit(1);
+    }
+    printf("Digite o nome do aluno: ");
+    scanf(" %[^\n]", estudante->nome);
+    printf("Informe a matricula: \n");
+    scanf("%d", &estudante->matricula);
+    printf("Informe o IRA:\n");
+    scanf("%f", &estudante->IRA);
+
+    return estudante;
+}
+
+void exibir_dados(Aluno * estudante){
+    printf("nome do aluno: %s\n", estudante->nome);
+    printf("matricula do aluno: %d\n", estudante->matricula);
+    printf("ira do aluno: %.2f\n", estudante->IRA);
+
+}
+
+void liberar_memoria(Aluno * estudante){
+    free(estudante);
+}
