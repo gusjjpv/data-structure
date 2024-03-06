cd7d885768b9fafc24092825f749dac44733671e
gusDev <joaogustavolima084@gmail.com>
2024-03-05 22:09:15 -0300

    exemplo aula

---
+++ 2° unidade/TAD/teoria/funcCalculadora.h
@@ -0,0 +1,11 @@
+//realiza a operacao de adicao
+int adicao(int n1, int n2);
+//realiza a operacao de subtracao
+int subtracao(int n1, int n2);
+//realiza a operacao de multiplicacao
+int multiplicacao(int n1, int n2);
+//realiza a operacao de divisao
+float divisao(int n1, int n2);
+
+//realiza o calculo de potenciacao
+int potenciacao(int n1, int n2);
